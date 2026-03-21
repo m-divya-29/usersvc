d1b331b2d1961be8ecfd3bc082d772066d2adc28
m-divya-29 <divya.gracie@gmail.com>
2026-03-21 15:33:22 +0530

    Signup with no authentication

---
+++ src/main/java/com/example/usersvc/Configurations/SecurityConfiguration.java
@@ -0,0 +1,18 @@
+package com.example.usersvc.Configurations;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfiguration {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) {
+        http.authorizeHttpRequests( auth ->
+            auth.anyRequest().permitAll()
+        ).csrf(csrf -> csrf.disable());
+        return http.build();
+    }
+
+}
